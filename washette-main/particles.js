eba4fbd5fa0a26b3555b7a22adcc2a2bca9691de
Veiyaeya <beatrizgailsolis@gmail.com>
2025-06-13 12:06:39 +0800

    Add files via upload

---
+++ washette-main/particles.js
@@ -0,0 +1,70 @@
+tsParticles.load("tsparticles", {
+  particles: {
+    number: {
+      value: 13,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: "#6CC1B6",
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100,
+      },
+    },
+    opacity: {
+      value: 0.28,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 20,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+      distance: 150,
+      color: "#7CDED2",
+      opacity: 0.22885281607335908,
+      width: 2.5252724532232724,
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "top",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+});
